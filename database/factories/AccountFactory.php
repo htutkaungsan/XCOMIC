9a6c1aa251f1c97e177345ad09002eb2194227db
Ggwp57 <htutkaungsan316@gmail.com>
2022-02-12 17:54:37 +0630

    gg

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Account;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+
+            
+        ];
+    }
+}
